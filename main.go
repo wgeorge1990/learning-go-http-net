e61c7f4860a6ba37ee006b0c4c54ed509b4b9f78
wgeorge1990 <williameverettgeorge@icloud.com>
2019-12-01 12:35:44 -0500

    Request breakdown

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 //build server
@@ -17,7 +18,24 @@ func (p person) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+
+	data := struct {
+		Method string
+		URL *url.URL
+		Submissions map[string][]string
+		Header http.Header
+		Host string
+		ContentLength int64
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+		req.Host,
+		req.ContentLength,
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
 var tpl *template.Template
